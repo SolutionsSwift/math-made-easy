bbec3527ef93bbc638609366a5a86ab5d6d35121
Stephan Hovius <shovius1992@gmail.com>
2018-07-30 22:45:51 +0200

    Added the first function sqrt.js

---
+++ sqrt.js
@@ -0,0 +1,9 @@
+/**
+ * This functions return the square root of the value its given.
+ * @param value
+ * @returns {number}
+ */
+export const sqrt = (value) => {
+    return Math.sqrt(value)
+};
+
